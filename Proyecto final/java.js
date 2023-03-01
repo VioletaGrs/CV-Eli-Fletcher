d1f0e4cba20fdc167f402e8da109b7d2a27473cf
VioletaGrs <126672292+VioletaGrs@users.noreply.github.com>
2023-03-01 10:21:54 -0300

    Add files via upload

--- Proyecto final/java.js
+++ Proyecto final/java.js
@@ -1,4 +1,4 @@
 document.getElementById("boton").onclick = function () {
 console.log("Capturamos el vento click");
-document.getElementById("demo").innerHTML = "Mi ubicación actual es en Buenos Aires, Argentina"
+document.getElementById("demo").innerHTML = "Mi ubicación actual es en Buenos Aires, Argentina. Nací en Estados Unidos pero viví una parte de mi vida en Argentina. Esto me permite manejar tanto el idioma inglés como español. Me concidero una persona proactiva y estoy en busqueda de un proyecto adecuado que me permita desarrollar mi experiencia laboral y capacidad para el logro de los objetivos organizacionales, como así también capacitarme en las áreas que sean posibles dentro del rubro alimentario. "
 }
