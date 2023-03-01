05d8c63d9ddc345423fd40e9c505a3caef580e3a
VioletaGrs <126672292+VioletaGrs@users.noreply.github.com>
2023-03-01 10:13:10 -0300

    Add files via upload

---
+++ Proyecto final/java.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+console.log("Capturamos el vento click");
+document.getElementById("demo").innerHTML = "Mi ubicación actual es en Buenos Aires, Argentina"
+}
